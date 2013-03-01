be0265aac1759cfe02bc58f4680df03dc079e906
Ershov-Aleksandr <ershovsasha@list.ru>
2013-03-01 21:21:48 +0400

    выделил ListElement в отдельный файл

--- 2 sem/(3) 26.02.13/2/2/List.cs
+++ 2 sem/(3) 26.02.13/2/2/List.cs
@@ -6,34 +6,6 @@ using System.Threading.Tasks;
 
 namespace _2
 {
-    public class ListElement
-    {
-        private int num;
-
-        public int GetNum()
-        {
-            return num;
-        }
-
-        public void SetNum(int num)
-        {
-            this.num = num;
-        }
-
-        private  ListElement next;
-
-        public ListElement GetNext()
-        {
-            return next;
-        }
-
-        public void SetNext(ListElement value)
-        {
-            this.next = value;
-        }
-
-    }
-
     public class List
     {
         private ListElement head;
---
+++ 2 sem/(3) 26.02.13/2/2/ListElement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    public class ListElement
+    {
+        private int num;
+
+        public int GetNum()
+        {
+            return num;
+        }
+
+        public void SetNum(int num)
+        {
+            this.num = num;
+        }
+
+        private ListElement next;
+
+        public ListElement GetNext()
+        {
+            return next;
+        }
+
+        public void SetNext(ListElement value)
+        {
+            this.next = value;
+        }
+    }
+}
