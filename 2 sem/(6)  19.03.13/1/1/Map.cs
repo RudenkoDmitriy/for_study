6ab9b17eed946cbe640f3819f5036619701236f1
Ershov-Aleksandr <ershovsasha@list.ru>
2013-03-20 10:17:22 +0400

    Map function with delegat

---
+++ 2 sem/(6)  19.03.13/1/1/Map.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _1
+{
+    public class Map
+    {
+        public delegate int ListFunction(int number);
+
+        public static List<int> MapFunc(List<int> list, ListFunction func)
+        {
+            for (int i = 0; i < list.Count(); ++i)
+            {
+                list[i] = func(list[i]);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ 2 sem/(6)  19.03.13/1/MapTests/MapTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1;
+using System.Collections.Generic;
+
+namespace MapTests
+{
+    [TestClass]
+    public class MapTests
+    {
+        private List<int> list;
+        private List<int> listResult;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            list = null;
+            listResult = null;
+        }
+
+        [TestMethod]
+        public void TestLenght()
+        {
+            listResult = new List<int> { 2, 4, 6 };
+            list = Map.MapFunc(new List<int> { 1, 2, 3 }, x => x * 2);
+            Assert.AreEqual(list.Count, listResult.Count);
+        }
+
+        [TestMethod]
+        public void CountTest_1()
+        {
+            listResult = new List<int> { 2, 4, 6 };
+            list = Map.MapFunc(new List<int> { 1, 2, 3 }, x => x * 2);
+
+            for (int i = 0; i < list.Count; ++i)
+            {
+                Assert.AreEqual(list[i], listResult[i]);
+            }
+        }
+
+        [TestMethod]
+        public void CountTest_2()
+        {
+            listResult = new List<int> { 7, 7, 7, 7 };
+            list = Map.MapFunc(new List<int> { 1, 2, 3, 5 }, x => 7);
+
+            for (int i = 0; i < list.Count; ++i)
+            {
+                Assert.AreEqual(list[i], listResult[i]);
+            }
+        }
+
+
+    }
+}
