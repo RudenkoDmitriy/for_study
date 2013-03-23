fbbdd0334063eb4889d4d681a40bd8a25ded3660
Ershov-Aleksandr <ershovsasha@list.ru>
2013-03-24 00:47:31 +0400

    corrected map function

--- 2 sem/(6)  19.03.13/1/1/Map.cs
+++ 2 sem/(6)  19.03.13/1/1/Map.cs
@@ -1,20 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+
 
 namespace _1
 {
     public class Map
     {
-        public delegate int ListFunction(int number);
-
-        public static List<int> MapFunc(List<int> list, ListFunction func)
+        public static List<int> MapFunc(List<int> list, Func<int, int> func)
         {
+            List<int> temp = new List<int>();
             for (int i = 0; i < list.Count(); ++i)
             {
-                list[i] = func(list[i]);
+                temp.Add(func(list[i]));
             }
-
-            return list;
+            return temp;
         }
     }
 }
