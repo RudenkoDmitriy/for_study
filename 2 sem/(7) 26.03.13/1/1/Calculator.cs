02dab9c7dd04fe343e5d1354eb019ccb0f1c7e97
Ershov-Aleksandr <ershovsasha@list.ru>
2013-04-02 16:34:05 +0400

    Calculator on Windows Forms

---
+++ 2 sem/(7) 26.03.13/1/1/Calculator.cs
@@ -0,0 +1,107 @@
+﻿
+namespace _1
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+            sign = 'n';
+        }
+
+        /// <summary>
+        /// This function using for add two-digit three-digit etc number. 
+        /// It call if click '0' - '9'.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public int GetNewNumber(int num)
+        {
+            currentNumber = currentNumber * 10 + num;
+            return currentNumber;
+        }
+
+        /// <summary>
+        /// Assign current number = 0. This function call if click 'Reset'.
+        /// </summary>
+        public void Reset()
+        {
+            currentNumber = 0;
+        }
+
+        /// <summary>
+        /// Return current number.
+        /// </summary>
+        /// <returns></returns>
+        public int GetNumber()
+        {
+            return currentNumber;
+        }
+
+        /// <summary>
+        /// This function using for count. It call if click '='.
+        /// </summary>
+        /// <param name="sign"></param>
+        public void Count()
+        {
+            if (sign == 'n')
+                return;
+            int num1 = tempNumberForCount;
+            int num2 = currentNumber;
+            tempNumberForCount = 0;
+            if (this.sign == '-')
+                currentNumber = num1 - num2;
+            if (this.sign == '+')
+                currentNumber = num1 + num2;
+            if (this.sign == '*')
+                currentNumber = num1 * num2;
+            if (this.sign == '/')
+                currentNumber = num1 / num2;
+            sign = 'n';
+        }
+
+        /// <summary>
+        /// This function call if click '+'.
+        /// </summary>
+        public void Add()
+        {
+            AddNewNumber();
+            sign = '+';
+        }
+
+        /// <summary>
+        /// This function call if click '-'.
+        /// </summary>
+        public void Subtract()
+        {
+            AddNewNumber();
+            sign = '-';
+        }
+
+        /// <summary>
+        /// This function call if click '*'.
+        /// </summary>
+        public void Miltiply()
+        {
+            AddNewNumber();
+            sign = '*';
+        }
+
+        /// <summary>
+        /// This function call if click '/'.
+        /// </summary>
+        public void Divide()
+        {
+            AddNewNumber();
+            sign = '/';
+        }
+
+        private void AddNewNumber()
+        {
+            tempNumberForCount = currentNumber;
+            currentNumber = 0;
+        }
+        private int currentNumber;
+        private int tempNumberForCount;
+        private char sign;
+    }
+}
---
+++ 2 sem/(7) 26.03.13/1/1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            calculator = new Calculator();
+            InitializeComponent();          
+        }
+
+        private void button_1_Click(object sender, EventArgs e)
+        {
+            calculator.GetNewNumber(1);
+            textBox.Text = calculator.GetNumber().ToString();
+        }
+
+        private Calculator calculator;
+
+        private void button_2_Click(object sender, EventArgs e)
+        {
+            calculator.GetNewNumber(2);
+            textBox.Text = calculator.GetNumber().ToString();
+        }
+
+        private void button_3_Click(object sender, EventArgs e)
+        {
+            calculator.GetNewNumber(3);
+            textBox.Text = calculator.GetNumber().ToString();
+        }
+
+        private void button_4_Click(object sender, EventArgs e)
+        {
+            calculator.GetNewNumber(4);
+            textBox.Text = calculator.GetNumber().ToString();
+        }
+
+        private void button_5_Click(object sender, EventArgs e)
+        {
+            calculator.GetNewNumber(5);
+            textBox.Text = calculator.GetNumber().ToString();
+        }
+
+        private void button_6_Click(object sender, EventArgs e)
+        {
+            calculator.GetNewNumber(6);
+            textBox.Text = calculator.GetNumber().ToString();
+        }
+
+        private void button_7_Click(object sender, EventArgs e)
+        {
+            calculator.GetNewNumber(7);
+            textBox.Text = calculator.GetNumber().ToString();
+        }
+
+        private void button_8_Click(object sender, EventArgs e)
+        {
+            calculator.GetNewNumber(8);
+            textBox.Text = calculator.GetNumber().ToString();
+        }
+
+        private void button_9_Click(object sender, EventArgs e)
+        {
+            calculator.GetNewNumber(9);
+            textBox.Text = calculator.GetNumber().ToString();
+        }
+
+        private void button_0_Click(object sender, EventArgs e)
+        {
+            calculator.GetNewNumber(0);
+            textBox.Text = calculator.GetNumber().ToString();
+        }
+
+        private void button_reset_Click(object sender, EventArgs e)
+        {
+            calculator.Reset();
+            textBox.Text = calculator.GetNumber().ToString();
+        }
+
+        private void button_result_Click(object sender, EventArgs e)
+        {
+            calculator.Count();
+            textBox.Text = calculator.GetNumber().ToString();
+        }
+
+        private void button_plus_Click(object sender, EventArgs e)
+        {
+            calculator.Add();
+        }
+
+        private void button_divide_Click(object sender, EventArgs e)
+        {
+            calculator.Divide();
+        }
+
+        private void button_multiply_Click(object sender, EventArgs e)
+        {
+            calculator.Miltiply();
+        }
+
+        private void button_minus_Click(object sender, EventArgs e)
+        {
+            calculator.Subtract();
+        }
+    }
+}
