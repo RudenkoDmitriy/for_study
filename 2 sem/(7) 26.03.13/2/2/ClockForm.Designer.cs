bf615fbc2e20c90edcd5c6c498f48deb1c63bc3e
Ershov-Aleksandr <ershovsasha@list.ru>
2013-04-07 22:30:12 +0400

    uploaded files

---
+++ 2 sem/(7) 26.03.13/2/2/ClockForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace _2
+{
+    partial class ClockForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerForClock = new System.Windows.Forms.Timer(this.components);
+            this.clock = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timerForClock
+            // 
+            this.timerForClock.Enabled = true;
+            this.timerForClock.Interval = 1;
+            this.timerForClock.Tick += new System.EventHandler(this.timerForClock_Tick);
+            // 
+            // clock
+            // 
+            this.clock.AutoSize = true;
+            this.clock.BackColor = System.Drawing.SystemColors.Info;
+            this.clock.Font = new System.Drawing.Font("Arial Rounded MT Bold", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clock.Location = new System.Drawing.Point(10, 10);
+            this.clock.Margin = new System.Windows.Forms.Padding(0);
+            this.clock.Name = "clock";
+            this.clock.Size = new System.Drawing.Size(50, 77);
+            this.clock.TabIndex = 0;
+            this.clock.Text = " \r\n";
+            // 
+            // ClockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(339, 105);
+            this.Controls.Add(this.clock);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ClockForm";
+            this.Text = "Clock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerForClock;
+        private System.Windows.Forms.Label clock;
+
+    }
+}
+
---
+++ 2 sem/(7) 26.03.13/2/2/ClockForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2
+{
+    public partial class ClockForm : Form
+    {
+        public ClockForm()
+        {
+            InitializeComponent();
+        }
+
+        private void timerForClock_Tick(object sender, EventArgs e)
+        {
+            clock.Text = DateTime.Now.Hour.ToString() + "." + DateTime.Now.Minute.ToString() + "." + DateTime.Now.Second.ToString();
+        }
+    }
+}
