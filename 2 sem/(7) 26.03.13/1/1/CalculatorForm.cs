009511010a5bbed4b5edb7b7a2e5d0818e97f219
Ershov-Aleksandr <ershovsasha@list.ru>
2013-04-07 20:10:02 +0400

    uploaded calculator tests.

---
+++ 2 sem/(7) 26.03.13/1/1/CalculatorForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class CalculatorForm : Form
+    {
+        
+        public CalculatorForm()
+        {
+            calculator = new Calculator();
+            InitializeComponent();          
+        }
+
+        private void button_Number_Click(object sender, EventArgs e)
+        { 
+            int num = Convert.ToInt32(sender.ToString()[sender.ToString().Length - 1]) - 48;
+            calculator.GetNewNumber(num);
+            textBox.Text = calculator.GetNumber().ToString();
+        }
+
+        private void button_reset_Click(object sender, EventArgs e)
+        {
+            calculator.Reset();
+            textBox.Text = calculator.GetNumber().ToString();
+        }
+
+        private void button_result_Click(object sender, EventArgs e)
+        {
+            calculator.Count();
+            textBox.Text = calculator.GetNumber().ToString();
+        }
+
+        private void button_plus_Click(object sender, EventArgs e)
+        {
+            calculator.Add();
+        }
+
+        private void button_divide_Click(object sender, EventArgs e)
+        {
+            calculator.Divide();
+        }
+
+        private void button_multiply_Click(object sender, EventArgs e)
+        {
+            calculator.Miltiply();
+        }
+
+        private void button_minus_Click(object sender, EventArgs e)
+        {
+            calculator.Subtract();
+        }
+
+        private Calculator calculator;
+    }
+}
---
+++ 2 sem/(7) 26.03.13/1/CalculatorTest.cs/CalculatorTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1;
+
+namespace CalculatorTest.cs
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            calc = new Calculator();
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            calc.GetNewNumber(1);
+            calc.GetNewNumber(2);
+            calc.Add();
+            calc.GetNewNumber(3);
+            calc.GetNewNumber(0);
+            calc.GetNewNumber(0);
+            calc.Count();
+            Assert.AreEqual(312, calc.GetNumber());
+        }
+
+        [TestMethod]
+        public void TestSubtract()
+        {
+            calc.GetNewNumber(1);
+            calc.Subtract();
+            calc.GetNewNumber(1);
+            calc.GetNewNumber(0);
+            calc.GetNewNumber(1);
+            calc.Count();
+            Assert.AreEqual(-100, calc.GetNumber());
+        }
+
+        [TestMethod]
+        public void TestMultiply()
+        {
+            calc.GetNewNumber(1);
+            calc.GetNewNumber(0);
+            calc.Miltiply();
+            calc.GetNewNumber(2);        
+            calc.GetNewNumber(1);
+            calc.Count();
+            Assert.AreEqual(210, calc.GetNumber());
+        }
+
+        [TestMethod]
+        public void TestDivide()
+        {
+            calc.GetNewNumber(1);
+            calc.GetNewNumber(0);
+            calc.GetNewNumber(2);
+            calc.Divide();
+            calc.GetNewNumber(2);
+            calc.Count();
+            Assert.AreEqual(51, calc.GetNumber());
+        }
+
+        [TestMethod]
+        public void TestAddThreeNumber()
+        {
+            calc.GetNewNumber(1);
+            calc.GetNewNumber(0);
+            calc.Add();
+            calc.GetNewNumber(2);
+            calc.GetNewNumber(1);
+            calc.Add();
+            calc.GetNewNumber(9);
+            calc.Count();
+            Assert.AreEqual(40, calc.GetNumber());
+        }
+
+        [TestMethod]
+        public void TestMultiplyThreeNumber()
+        {
+            calc.GetNewNumber(1);
+            calc.GetNewNumber(0);
+            calc.Miltiply();
+            calc.GetNewNumber(2);
+            calc.GetNewNumber(1);
+            calc.Miltiply();
+            calc.GetNewNumber(2);
+            calc.Count();
+            Assert.AreEqual(420, calc.GetNumber());
+        }
+
+        private Calculator calc;
+    }
+}
