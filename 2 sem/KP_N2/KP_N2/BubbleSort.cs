19de4f890ffb771434d11450a169e1dbd64403f7
Ershov-Aleksandr <ershovsasha@list.ru>
2013-05-21 14:55:09 +0400

    KP

---
+++ 2 sem/KP_N2/KP_N2/BubbleSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KP_N2
+{
+    public class BubbleSort<T>
+    {
+        /// <summary>
+        /// This function using for sort array.
+        /// </summary>
+        /// <param name="array"></param>
+        public static void Sort(ref T[] array)
+        {
+            for (int i = 0; i < array.Length - 1; ++i)
+                for (int j = i + 1; j < array.Length; ++j)
+                {
+                    if (Compare(array[i], array[j]))
+                    {
+                        T temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+        }
+
+        private static bool Compare(T item1, T item2)
+        {
+            int h1 = item1.GetHashCode();
+            int h2 = item2.GetHashCode();
+            return h1 > h2;
+        }   
+    }
+}
---
+++ 2 sem/KP_N2/SortTest/SortTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KP_N2;
+
+namespace SortTest
+{
+    [TestClass]
+    public class SortTest
+    {        
+        [TestMethod]
+        public void TestChar()
+        {
+            char[] arr = {'h', 'a', 'd', 'b', 'c', 'e'};
+            BubbleSort<char>.Sort(ref arr);
+            Assert.AreEqual('a', arr[0]);
+            Assert.AreEqual('b', arr[1]);
+            Assert.AreEqual('c', arr[2]);
+            Assert.AreEqual('d', arr[3]);
+            Assert.AreEqual('e', arr[4]);
+            Assert.AreEqual('h', arr[5]);
+        }
+
+        [TestMethod]
+        public void TestInt()
+        {
+            int[] arr = { 34, 56, 34, 78, 0, 14, 90 };
+            BubbleSort<int>.Sort(ref arr);
+            Assert.AreEqual(0, arr[0]);
+            Assert.AreEqual(14, arr[1]);
+            Assert.AreEqual(34, arr[2]);
+            Assert.AreEqual(34, arr[3]);
+            Assert.AreEqual(56, arr[4]);
+            Assert.AreEqual(78, arr[5]);
+            Assert.AreEqual(90, arr[6]);
+        }
+
+        [TestMethod]
+        public void TestBigInt()
+        {
+            int[] arr = new int[100];
+            for (int i = 0; i < 100; ++i)
+            {
+                arr[i] = 99 - i;
+            }
+            BubbleSort<int>.Sort(ref arr);
+            for (int i = 0; i < 100; ++i)
+            {
+                Assert.AreEqual(i, arr[i]);
+            }
+        }
+    }
+}
