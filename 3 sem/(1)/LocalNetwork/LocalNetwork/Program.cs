4ff8b36ceb51a545123bae7a78df0673c0ea4c62
Ershov-Aleksandr <eershoov@gmail.com>
2013-09-17 23:49:03 +0400

    Network without tests.

---
+++ 3 sem/(1)/LocalNetwork/LocalNetwork/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LocalNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Network net = new Network("..\\..\\..\\test1.txt");
+            net.Show();
+            net.Step();
+            net.Step();
+            net.Step();
+            net.Step();
+            net.Step();
+            net.Show();
+            net.Step();
+            net.Step();
+            net.Step();
+            net.Show();
+            Console.WriteLine("*********");
+            net = new Network("..\\..\\..\\test2.txt");
+            net.Show();
+            net.Step();
+            net.Step();
+            net.Step();
+            net.Step();
+            net.Step();
+            net.Show();
+            net.Step();
+            net.Step();
+            net.Step();
+            net.Show();
+            Console.ReadKey();
+        }
+    }
+}
