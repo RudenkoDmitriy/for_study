93c9721ba8d574c1809ed196e8b362f3b08f07e3
Ershov-Aleksandr <eershoov@gmail.com>
2013-09-20 13:29:45 +0400

    int matrix -> bool matrix deleted "ref"

--- 3 sem/1/LocalNetwork/LocalNetwork/Network.cs
+++ 3 sem/1/LocalNetwork/LocalNetwork/Network.cs
@@ -18,12 +18,10 @@ namespace LocalNetwork
             StreamReader file = new System.IO.StreamReader(path);
             this.numberOfComputers = file.Read() - 48;
             if (this.numberOfComputers < 1)
-            {
                 throw new IncorrectInputException();
-            }
             file.ReadLine();
             this.listOfComputers = new Computer[this.numberOfComputers];
-            this.matrix = new int[this.numberOfComputers][];
+            this.matrix = new bool[this.numberOfComputers][];
             for (int i = 0; i < this.numberOfComputers; ++i)
             {
                 string temp = file.ReadLine();
@@ -31,23 +29,20 @@ namespace LocalNetwork
                 int isInfect = file.Read() - 48;
                 file.ReadLine();
                 if ((os != 'w' && os != 'l' && os != 'm') || (isInfect != 0 && isInfect != 1))
-                {
                     throw new IncorrectInputException();
-                }
                 this.listOfComputers[i] = new Computer(os, isInfect);
             }
             string[] matr = file.ReadLine().Split(' ');
             int count = 0;
             for (int i = 0; i < this.numberOfComputers; ++i)
             {
-                this.matrix[i] = new int[this.numberOfComputers];
+                this.matrix[i] = new bool[this.numberOfComputers];
                 for (int j = 0; j < this.numberOfComputers; ++j)
                 {
-                    matrix[i][j] = matr[count][0] - 48;
-                    if (matrix[i][j] != 0 && matrix[i][j] != 1)
-                    {
+                    int temp = matr[count][0] - 48;
+                    if (temp !=0 && temp != 1)
                         throw new IncorrectInputException();
-                    }
+                    matrix[i][j] = temp == 1;
                     count++;                  
                 }
             }
@@ -60,10 +55,8 @@ namespace LocalNetwork
         {
             bool[] visited = new bool[numberOfComputers];
             for (int i = 0; i < this.numberOfComputers; ++i)
-            {
                 visited[i] = false;
-            }
-            this.Traverse(0, false, ref  visited);
+            this.Traverse(0, false, visited);
         }
 
         /// <summary>
@@ -75,13 +68,9 @@ namespace LocalNetwork
             {
                 string inf;
                 if (listOfComputers[i].Virus)
-                {
                     inf = " infected.";
-                }
                 else
-                {
                     inf = " not infected.";
-                }
                 string temp = i + 1 + ") " + listOfComputers[i].OperationSystem + inf;
                 Console.WriteLine(temp);
             }
@@ -89,21 +78,17 @@ namespace LocalNetwork
         }
 
         private Computer[] listOfComputers;
-        private int[][] matrix;
+        private bool[][] matrix;
         private int numberOfComputers;
-        private void Traverse(int numberOfComputer, bool isInfection, ref bool[] visited)
+        private void Traverse(int numberOfComputer, bool isInfection, bool[] visited)
         {
             visited[numberOfComputer] = true;
             if (isInfection)
-            {
                 this.listOfComputers[numberOfComputer].Infection();
-            }
             for (int i = 0; i < this.numberOfComputers; ++i)
             {
-                if (!visited[i] && (this.matrix[numberOfComputer][i] == 1))
-                {
-                    this.Traverse(i, this.listOfComputers[numberOfComputer].Virus, ref visited);
-                }
+                if (!visited[i] && this.matrix[numberOfComputer][i])
+                    this.Traverse(i, this.listOfComputers[numberOfComputer].Virus, visited);
             }
         }
     }
